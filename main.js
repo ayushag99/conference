c9ecb5fd2b7254afc47ad441d4486a9656340c3a
Ayush Agrawal <ayush.ashuvaish@gmail.com>
2019-08-13 14:10:13 +0530

    This is the initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+// Scroll animation
+// window.addEventListener("scroll", ()=>{
+//     let distance_fromTop = document.getElementById("title").offsetTop;
+//     console.log(distance_fromTop, window.pageYOffset)
+//     h2 = document.getElementById("title");
+//     if(window.pageYOffset >= distance_fromTop)
+//         h2.style.display = "none"
+
+// })
